28f666c62fd147cb411c8f7e0b5158e2be0ea4a1
Susino <t.sygut@clickray.pl>
2021-07-01 11:36:14 +0200

    Environment and first routes

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Switch, Route} from 'react-router-dom'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <Switch>
+
+    <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/register" component={Register} /> 
+
+    </Switch>
+  )
+};
+ 
+export default App;
+  

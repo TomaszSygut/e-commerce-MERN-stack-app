368a7aeb6c35fa43f34d19e9e6ed0337c74d75dd
Susino <t.sygut@clickray.pl>
2021-07-01 17:32:18 +0200

    setup redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
 import {toast, ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
-import Home from "./pages/Home";
+import Home from "./pages/Home"; 
 import Header from "./components/nav/Header";
+import RegisterComplete from './pages/auth/RegisterComplete'
 
 
 const App = () => {
@@ -19,6 +18,7 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/register/complete" component={RegisterComplete} />
       </Switch>
     </>
   );

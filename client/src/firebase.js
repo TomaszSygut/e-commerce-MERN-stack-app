05057cc255a4b29419b4af993e73bfac66dc12c0
Susino <t.sygut@clickray.pl>
2021-07-01 15:03:19 +0200

    Register Input styles and logic

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,8 +1,8 @@
-  import * as firebase from 'firebase'
-  
+import firebase from "firebase/app";
+import "firebase/auth";
   
   // Your web app's Firebase configuration
-  const firebaseConfig = {
+  const config = {
     apiKey: "AIzaSyA-n1Gn6cCgbMn8wc5fbdM9X_qDPeyIwlg",
     authDomain: "ecommerce-a9075.firebaseapp.com",
     projectId: "ecommerce-a9075",
@@ -11,9 +11,10 @@
     appId: "1:673490560415:web:60cf56518124831e8ac9aa"
   };
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
+ 
   // export
-
-  export const auth = firebase.auth()
-  export const googleAuthProvider = new firebase.auth.googleAuthProvider();
+  export const auth = firebase.auth(); 
+  export const googleAuthProvider = new  firebase.auth.GoogleAuthProvider();
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,16 +1,42 @@
 import React, { useState } from "react";
+import {auth} from '../../firebase'
+import {toast, ToastContainer} from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.css'
 
 const Register = () => {
   const [email, setEmail] = useState("");
 
-  const handleSubmit = () => {};
+
+
+
+  const handleSubmit = async (e) => { 
+   e.preventDefault()
+
+   const config = {
+       url: 'http://localhost:3000/register/complete',
+       handleCodeInApp: true
+   } 
+
+   await auth.sendSignInLinkToEmail(email, config)
+
+   toast.success(`Email is sent to ${email}, Click the link to complete your registration.`
+   ); 
+
+   window.localStorage.setItem('emailForRegistration', email)
+
+   setEmail("");
+  };
+
+
+
   const registerForm = () => (
     <form onSubmit={handleSubmit}>
       <input
         type="email"
         className="form-control"
         value={email}
-        onChange={(e) => console.log(e.target.value)}
+        onChange={(e) => setEmail(e.target.value)}
         autoFocus
       />
       <button type="submit" className="btn btn-raised"> Register</button>

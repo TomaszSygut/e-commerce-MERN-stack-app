f7a46927a6cf6d0bb4a39f2fa55258d80a0f02ac
Susino <thomassygut@gmail.com>
2021-07-02 16:28:41 +0200

    redirect from /logn /register

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth, googleAuthProvider } from "../../firebase";
 import { toast } from "react-toastify";
 import { Button } from "antd";
 import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
-import { useDispatch } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
 import {Link} from 'react-router-dom'
 
 const Login = ({ history }) => {
   const [email, setEmail] = useState("thomassygut@gmail.com");
   const [password, setPassword] = useState("Menelpl666");
   const [loading, setLoading] = useState(false);
+  const {user} = useSelector(state => ({...state}))
+
+  useEffect(() => {
+    if(user && user.token) {
+      history.push("/")
+    }
+  }, [user])
 
   let dispatch = useDispatch();
 

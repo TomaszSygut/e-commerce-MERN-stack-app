153f5bfa7dd188313b6bb77ea4aed5f2f943783f
Susino <t.sygut@clickray.pl>
2021-07-02 15:58:33 +0200

    tescik

--- client/src/pages/auth/ForgotPassword.js
+++ client/src/pages/auth/ForgotPassword.js
@@ -10,7 +10,7 @@ const ForgotPassword = ({ history }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-
+//dddd
     const config = {
       url: "http://localhost:3000/login",
       handleCodeInApp: true,

5e79f2afb48e85404926525543fd88ff8c375f16
Susino <thomassygut@gmail.com>
2021-07-05 16:36:32 +0200

    Redirect Countdown

--- client/src/components/routes/UserRoute.js
+++ client/src/components/routes/UserRoute.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, LInk } from "react-router-dom";
 import { useSelector } from "react-redux";
+import LoadingToRedirect from './LoadingToRedirect'
 
 const UserRoute = ({ children, ...rest }) => {
   const { user } = useSelector((state) => ({ ...state }));
@@ -8,7 +9,7 @@ const UserRoute = ({ children, ...rest }) => {
   return user && user.token ? (
     <Route {...rest} render={() => children}></Route>
   ) : (
-    <h1 className="text-danger"> Loading ...</h1>
+   <LoadingToRedirect/>
   );
 };
 

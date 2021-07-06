d4a57119ec92689e2159a539e110efd0567ab0e8
Susino <thomassygut@gmail.com>
2021-07-06 14:34:53 +0200

    create category

---
+++ server/controllers/category.js
@@ -0,0 +1,30 @@
+const Category = require("../models/category");
+const slugify = require("slugify");
+
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    // const category = await new Category({ name, slug: slugify(name) }).save();
+    // res.json(category);
+    res.json(await new Category({ name, slug: slugify(name) }).save());
+  } catch (err) {
+    // console.log(err);
+    res.status(400).send("Create category failed");
+  }
+};
+
+exports.list = async (req, res) =>{
+
+}
+ 
+exports.read = async (req, res) => {
+
+};
+
+exports.update = async (req, res) => {
+
+};
+
+exports.remove = async (req, res) => {
+
+};

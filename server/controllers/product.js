e7eababa92da6a61504728ca510fbca2aaaba446
Susino <thomassygut@gmail.com>
2021-07-07 15:28:25 +0200

    Product Create-Backend

---
+++ server/controllers/product.js
@@ -0,0 +1,13 @@
+const Product = require("../models/product");
+const slugify = require("slugify");
+
+exports.create = async (req, res) => {
+  try {
+    console.log(req.body);
+    req.body.slug = slugify(req.body.title);
+    const newProduct = await new Product(req.body).save();
+    res.json(newProduct);
+  } catch (error) {
+    res.status(400).send("Create product failed");
+  }
+};
---
+++ server/routes/product.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+// middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+// controller
+const { create } = require("../controllers/product");
+
+// routes
+router.post("/product", authCheck, adminCheck, create);
+
+module.exports = router;

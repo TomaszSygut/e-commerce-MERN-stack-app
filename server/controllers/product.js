f721fede57eb55d75652670f5f4a03d6be14ac2c
Susino <thomassygut@gmail.com>
2021-07-09 11:25:41 +0200

    product list

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -20,3 +20,13 @@ exports.read = async (req, res) => {
   let products = await Product.find({});
   res.json(products);
 };
+
+exports.listAll = async (req, res) => {
+  let products = await Product.find({})
+    .limit(parseInt(req.params.count))
+    .populate("category")
+    .populate("subs")
+    .sort([["createdAt", "desc"]])
+    .exec();
+  res.json(products);
+};
--- server/routes/product.js
+++ server/routes/product.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const { authCheck, adminCheck } = require("../middlewares/auth");
 
 // controller
-const { create, read } = require("../controllers/product");
+const { create, listAll } = require("../controllers/product");
 
 // routes
 router.post("/product", authCheck, adminCheck, create);
-router.get("/products", read);
+router.get("/products/:count", listAll); // products 100
 
 module.exports = router;

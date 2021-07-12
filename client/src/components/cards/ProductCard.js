7d50e1a68d45513fcb73dd44b31fef46d50ce212
Susino <thomassygut@gmail.com>
2021-07-12 16:20:16 +0200

    loading card

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Skeleton } from "antd";
 import laptop from "../../images/laptop.jpeg";
 import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
@@ -9,28 +9,30 @@ const ProductCard = ({ product }) => {
   const { title, description, images, slug } = product;
   const { Meta } = Card;
   return (
-    <Card
-      cover={
-        <img
-          src={images && images.length ? images[0].url : laptop}
-          style={{ height: "150px", objectFit: "cover" }}
-          className="p-1"
+    <>
+      <Card
+        cover={
+          <img
+            src={images && images.length ? images[0].url : laptop}
+            style={{ height: "150px", objectFit: "cover" }}
+            className="p-1"
+          />
+        }
+        actions={[
+          <Link to={`/product/${slug}`}>
+            <EyeOutlined className="text-warning" /> <br /> View Product
+          </Link>,
+          <>
+            <ShoppingCartOutlined className="text-danger" /> <br /> Add to Cart
+          </>,
+        ]}
+      >
+        <Meta
+          title={title}
+          description={`${description && description.substring(0, 40)}...`}
         />
-      }
-      actions={[
-        <Link to={`/product/${slug}`}>
-          <EyeOutlined className="text-warning" /> <br /> View Product
-        </Link>,
-        <>
-          <ShoppingCartOutlined className="text-danger" /> <br /> Add to Cart
-        </>,
-      ]}
-    >
-      <Meta
-        title={title}
-        description={`${description && description.substring(0, 40)}...`}
-      />
-    </Card>
+      </Card>
+    </>
   );
 };
 export default ProductCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import { getProductsByCount } from "../functions/product";
 import ProductCard from "../components/cards/ProductCard";
 import Jumbotron from "../components/cards/Jumbotron";
-
+import LoadingCard from "../components/cards/LoadingCard";
 const Home = () => {
   const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     loadAllProducts();
@@ -15,24 +15,28 @@ const Home = () => {
     setLoading(true);
     getProductsByCount(3).then((res) => {
       setProducts(res.data);
-      setLoading(false);
+      setLoading(true);
     });
   };
 
   return (
     <>
-      <div className="jumbotron">
+      <div className="jumbotron text-danger h1 font-weight-bold text-center">
         <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
       </div>
 
       <div className="container">
-        <div className="row">
-          {products.map((product) => (
-            <div key={product._id} className="col-md-4">
-              <ProductCard product={product} />
-            </div>
-          ))}
-        </div>
+        {loading ? (
+          <LoadingCard count={6} />
+        ) : (
+          <div className="row">
+            {products.map((product) => (
+              <div key={product._id} className="col-md-4 pb-5 pt-2">
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );

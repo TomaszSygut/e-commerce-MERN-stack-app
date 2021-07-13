d4bf4be96a7a941c9d4ee02009f1766591000760
Susino <thomassygut@gmail.com>
2021-07-13 11:02:22 +0200

    New Arrivals pagination

--- client/src/components/home/NewArrivals.js
+++ client/src/components/home/NewArrivals.js
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { getProducts } from "../../functions/product";
+import { getProducts, getProductsCount } from "../../functions/product";
 import ProductCard from "../cards/ProductCard";
 import Jumbotron from "../cards/Jumbotron";
 import LoadingCard from "../cards/LoadingCard";
+import { Pagination } from "antd";
+
 const NewArrivals = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [productsCount, setProductsCount] = useState(0);
 
   useEffect(() => {
     loadAllProducts();
+  }, [page]);
+
+  useEffect(() => {
+    getProductsCount().then((res) => {
+      setProductsCount(res.data);
+      console.log(res.data);
+    });
   }, []);
 
   const loadAllProducts = () => {
     setLoading(true);
     // sort, order , limit
-    getProducts("createdAt", "desc", 3).then((res) => {
+    getProducts("createdAt", "desc", page).then((res) => {
       setProducts(res.data);
       setLoading(false);
     });
@@ -35,6 +46,12 @@ const NewArrivals = () => {
           </div>
         )}
       </div>
+      <Pagination
+        current={page}
+        className="d-flex justify-content-center"
+        total={(productsCount / 3) * 10}
+        onChange={(value) => setPage(value)}
+      />
     </>
   );
 };

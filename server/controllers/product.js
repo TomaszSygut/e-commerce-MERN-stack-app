4a1d390d5c333ba8b1de30995bff0590a82304ff
Susino <thomassygut@gmail.com>
2021-07-13 10:27:08 +0200

    list products with pagination

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -65,15 +65,37 @@ exports.update = async (req, res) => {
     });
   }
 };
+// without pagination
+// exports.list = async (req, res) => {
+//   try {
+//     const { sort, order, limit } = req.body;
+//     const products = await Product.find({})
+//       .populate("category")
+//       .populate("subs")
+//       .sort([[sort, order]])
+//       .limit(limit)
+//       .exec();
+
+//     res.json(products);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// with pagination
 
 exports.list = async (req, res) => {
   try {
-    const { sort, order, limit } = req.body;
+    const { sort, order, page } = req.body;
+    const currentPage = page || 1;
+    const perPage = 3;
+
     const products = await Product.find({})
+      .skip((currentPage - 1) * perPage)
       .populate("category")
       .populate("subs")
       .sort([[sort, order]])
-      .limit(limit)
+      .limit(perPage)
       .exec();
 
     res.json(products);
@@ -81,7 +103,6 @@ exports.list = async (req, res) => {
     console.log(err);
   }
 };
-
 exports.productsCount = async (req, res) => {
   let total = await Product.find({}).estimatedDocumentCount().exec();
   res.json(total);
